c54dec25e287d2aa0495b0bb2a75989e06ce2227
JuanCGarciaA <juancarlos@develop.mx>
2022-12-02 12:34:20 -0600

    first commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { AuthRouthingModule } from './auth-routhing.module';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegistroComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRouthingModule
+  ]
+})
+export class AuthModule { }
